78bbbd8eab96905e4e2c407d1d6494155d37b12f
pogoparis <pogoparis@gmail.com>
2023-09-13 14:19:53 +0200

    miniature des images dans la liste des poules

--- app/src/main/java/com/example/poulailler/PouleAdapter.kt
+++ app/src/main/java/com/example/poulailler/PouleAdapter.kt
@@ -3,9 +3,10 @@ package com.example.poulailler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+import com.squareup.picasso.Picasso
 
 
 class PouleAdapter(
@@ -31,18 +32,31 @@ class PouleAdapter(
 
     override fun onBindViewHolder(holder: PouleAdapter.PouleViewHolder, position: Int) {
         val currentPoule = poules[position]
-        holder.nomTextView.text = currentPoule.nom
-        holder.raceTextView.text = currentPoule.race
-
+     /*   holder.nomTextView.text = currentPoule.nom
+        holder.raceTextView.text = currentPoule.race*/
+holder.bind(currentPoule)
     }
 
     inner class PouleViewHolder(itemView: View, clickListener: OnItemClickListener) : RecyclerView.ViewHolder(itemView) {
         val nomTextView: TextView = itemView.findViewById(R.id.nomTextView)
         val raceTextView: TextView = itemView.findViewById(R.id.raceTextView)
+        // Référence à votre ImageView dans le layout de l'élément de RecyclerView
+        private val avatarImageView: ImageView = itemView.findViewById(R.id.avatarPouleList)
 
         init {
             itemView.setOnClickListener{
                 clickListener.onItemClick(adapterPosition)
+            }
+        }
+        fun bind(poule: Poule) {
+            nomTextView.text = poule.nom
+            raceTextView.text = poule.race
+
+            // Charger et afficher l'image à partir de l'URL Firebase
+            if (poule.imageUrl!!.isNotEmpty()) {
+                Picasso.get().load(poule.imageUrl).into(avatarImageView)
+            } else {
+                avatarImageView.setImageResource(R.drawable.poule1)
             }
         }
     }
